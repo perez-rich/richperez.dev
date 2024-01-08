6859ca7b3271f98b971f8c898cb4f897e15ecb4c
Rich Perez <imperez5@gmail.com>
2024-01-07 21:07:27 -0500

    added intro message

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import Image from 'next/image';
 import './globals.css'
 
 export const metadata: Metadata = {
@@ -11,7 +12,14 @@ export const metadata: Metadata = {
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="en">
-      <body className="bg-slate-800 h-screen">{children}</body>
+      <body className="bg-slate-800 text-stone-200 h-screen">
+        <header className="flex justify-center">
+          <Image src="/img/site-logo.png" alt="imperez logo" width="300" height="86" />
+        </header>
+        <main>
+          {children}
+        </main>
+      </body>
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,13 @@ import Image from 'next/image';
 import {  } from "module";
 
 export default function Home() {
+  const now = new Date()
+  const years = now.getFullYear() - 2007;
+
   return (
-    <main>
-        <header className="flex justify-center">
-          <Image src="/img/site-logo.png" alt="imperez logo" width="300" height="86" />
-        </header>
-    </main>
+    <div className="mx-auto max-w-2xl font-mono text-2xl text-center">
+      <Image className="rounded-full mx-auto my-10" src="/img/rich.jpg" alt="s picture of rich perez" width="100" height="100" />
+      <p>Hello 👋🏽 my name is <span className="font-black text-white">Rich Perez</span> and I'm a full-stack developer with {years}+ years of experience.</p>
+    </div>
   )
 }
