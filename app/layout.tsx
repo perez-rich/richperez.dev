29a8a930c8644d3f028d7b2574fbba91eba80a15
Rich Perez <imperez5@gmail.com>
2024-01-07 21:14:33 -0500

    added linkedin anchor

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,10 +11,11 @@ export const metadata: Metadata = {
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
-    <html lang="en">
+    <html lang="en" data-theme="corporate">
       <body className="bg-slate-800 text-stone-200 h-screen">
-        <header className="flex justify-center">
+        <header className="flex justify-between px-4 border-b border-slate-500">
           <Image src="/img/site-logo.png" alt="imperez logo" width="300" height="86" />
+          <a className="btn btn-primary text-white my-4" href="https://www.linkedin.com/in/imperez/" target="_blank" >Connect on LinkedIn</a>
         </header>
         <main>
           {children}
