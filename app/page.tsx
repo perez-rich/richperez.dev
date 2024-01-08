620919f1862c6d1da953eb5f399b1401226f51a1
Rich Perez <imperez5@gmail.com>
2024-01-08 09:18:58 -0500

    5 add website intro (#8)

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,8 @@ export default function Home() {
 
   return (
     <div className="mx-auto max-w-2xl font-mono text-2xl text-center">
-      <Image className="rounded-full mx-auto my-10" src="/img/rich.jpg" alt="s picture of rich perez" width="100" height="100" />
-      <p>Hello 👋🏽 my name is <span className="font-black text-white">Rich Perez</span> and I'm a full-stack developer with {years}+ years of experience.</p>
+      <Image className="rounded-full mx-auto my-10" src="/img/rich.jpg" alt="picture of rich perez" width="100" height="100" />
+      <p>Hello 👋🏽 my name is <span className="font-black text-white">Rich Perez</span> and I am a full-stack developer with {years}+ years of experience.</p>
     </div>
   )
 }
