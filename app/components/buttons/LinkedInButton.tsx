4f5ea0a26307bc3289deabf935318bd412c17797
Rich Perez <imperez5@gmail.com>
2024-01-08 20:56:07 -0500

    added resume download to header (#12)

---
+++ app/components/buttons/LinkedInButton.tsx
@@ -0,0 +1,5 @@
+export default function () {
+    return <a className="btn btn-sm btn-primary text-white md:my-4" href="https://www.linkedin.com/in/imperez/" target="_blank" >
+        LinkedIn <i className="las la-external-link-alt"></i>
+    </a>;
+}
---
+++ app/components/buttons/ResumeButton.tsx
@@ -0,0 +1,5 @@
+export default function () {
+    return <a className="btn btn-sm btn-ghost text-white md:my-4" href="https://docs.google.com/document/d/1-X2XsNCaCy0j93hHALWcO9V_5NWPxGGYRBbh7p0eAxk/export?format=pdf" target="_blank">
+        Resume <i className="lar la-file-pdf"></i>
+    </a>;
+}
