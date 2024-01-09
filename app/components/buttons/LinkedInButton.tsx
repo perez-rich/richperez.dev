23bbf2189af0e0486cb005582fe4281c57014e03
Rich Perez <imperez5@gmail.com>
2024-01-08 20:59:16 -0500

    9 add resume pdf (#13)

--- app/components/buttons/LinkedInButton.tsx
+++ app/components/buttons/LinkedInButton.tsx
@@ -1,4 +1,4 @@
-export default function () {
+export default function LinkedInButton() {
     return <a className="btn btn-sm btn-primary text-white md:my-4" href="https://www.linkedin.com/in/imperez/" target="_blank" >
         LinkedIn <i className="las la-external-link-alt"></i>
     </a>;
--- app/components/buttons/ResumeButton.tsx
+++ app/components/buttons/ResumeButton.tsx
@@ -1,4 +1,4 @@
-export default function () {
+export default function ResumeButton() {
     return <a className="btn btn-sm btn-ghost text-white md:my-4" href="https://docs.google.com/document/d/1-X2XsNCaCy0j93hHALWcO9V_5NWPxGGYRBbh7p0eAxk/export?format=pdf" target="_blank">
         Resume <i className="lar la-file-pdf"></i>
     </a>;
