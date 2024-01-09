4e6d5419d9c5232de0da98c017b45fcd4da9a0ab
Rich Perez <imperez5@gmail.com>
2024-01-08 20:55:27 -0500

    moved buttons into their own components

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,15 +1,13 @@
 import Image from 'next/image';
+import LinkedInButton from './buttons/LinkedInButton';
+import ResumeButton from './buttons/ResumeButton';
 
 export default function () {
     return <header className="flex flex-col md:flex-row items-center md:justify-between px-4 border-b py-4 md:py-0 border-slate-500">
     <a href="/"><Image src="/img/site-logo.png" alt="imperez logo" width="300" height="86" /></a>
     <nav className="flex gap-2 justify-center">
-      <a className="btn btn-sm btn-primary text-white md:my-4" href="https://www.linkedin.com/in/imperez/" target="_blank" >
-        LinkedIn <i className="las la-external-link-alt"></i>
-      </a>
-      <a className="btn btn-sm btn-ghost text-white md:my-4" href="https://docs.google.com/document/d/1-X2XsNCaCy0j93hHALWcO9V_5NWPxGGYRBbh7p0eAxk/export?format=pdf" target="_blank">
-        Resume <i className="lar la-file-pdf"></i>
-      </a>
+      <LinkedInButton />
+      <ResumeButton />
     </nav>
   </header>;
 }
