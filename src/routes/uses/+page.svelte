db76c3ae65a3081d7e41b7d527bce7c0148bc917
Rich Perez <imperez5@gmail.com>
2024-06-28 09:41:40 -0400

    added uses page

---
+++ src/routes/uses/+page.svelte
@@ -0,0 +1,34 @@
+<script>
+    import PageHeading from "$lib/components/ui/PageHeading.svelte";
+</script>
+
+<svelte:head>
+    <title>Rich Perez - Uses - Web Developer</title>
+</svelte:head>
+
+<section class="mx-auto max-w-2xl font-mono text-xl md:text-2xl text-center py-10">
+    <PageHeading label="Uses" />
+    <p>I'm chronicling the apps, tools and programming languages that I use often.</p>
+</section>
+
+<section class="mx-auto max-w-2xl flex flex-col gap-2">
+    <h2 class="text-2xl font-bold font-mono">Coding Tools</h2>
+    <ul class="list-disc flex flex-col gap-2">
+        <li><a href="https://code.visualstudio.com/" target="_blank">Visual Studio Code</a> is my editor I use daily across multiple projects. I've also started trying out <a href="https://www.jetbrains.com/fleet/" target="_blank">Jetbrains Fleet</a> editor.</li>
+        <li>I use <a href="https://www.apple.com/safari/" target="_blank">Safari</a> as my main browser. I'm loving it's tight integration across Apple devices and of course the privacy features.</li>
+        <li>For REST APIs or dynamic websites I use <a href="https://laravel.com" target="_blank">Laravel</a> and <a href="https://www.php.net" target="_blank">PHP 8+</a> with some <a href="https://www.mysql.com" target="_blank">MySQL 8</a> thrown in.</li>
+        <li>For my pure frontend joys I use <a href="https://kit.svelte.dev" target="_blank">SvelteKit</a> with either vanilla <a href="https://www.w3.org/Style/CSS/Overview.en.html" target="_blank">CSS</a> or <a href="https://tailwindcss.com" target="_blank">Tailwind</a> & <a href="https://daisyui.com" target="_blank">DaisyUI</a> to make everything look nice.</li>
+        <li>For my API testing I really enjoy <a href="https://www.postman.com" target="_blank">Postman</a>. It's practically a standard at this point and very easy to use.</li>
+        <li>For the occasional mobile app development my editor is of course <a href="https://developer.apple.com/xcode/" target="_blank">Xcode</a>. The <a href="https://www.swift.org" target="_blank">Swift language</a> is really a joy to use.</li>
+    </ul>
+</section>
+
+<section class="mx-auto max-w-2xl flex flex-col gap-2 mt-10">
+    <h2 class="text-2xl font-bold font-mono">Computer & Devices</h2>
+    <ul class="list-disc flex flex-col gap-2">
+        <li>I work off my 14 inch M1 Macbook Pro. I do not use multiple monitors and opt for the single laptop screen.</li>
+        <li>My daily mobile device is an <a href="https://www.apple.com/iphone-15-pro/" target="_blank">iPhone 15 Pro</a> with the Blue Titanium finish.</li>
+        <li>For my health tracking needs I wear an <a href="https://www.apple.com/apple-watch-series-9/" target="_blank">Apple Watch Series 9</a>.</li>
+        <li>My streaming box of choice is an Apple TV 4K. I can't beat the ease of use and great app support.</li>
+    </ul>
+</section>
