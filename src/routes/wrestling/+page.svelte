24e313cf4ac3d34e017bd2684368f9b6270f5dfb
Rich Perez <imperez5@gmail.com>
2024-06-28 09:41:39 -0400

    added wrestling slash page

---
+++ src/routes/wrestling/+page.svelte
@@ -0,0 +1,38 @@
+<script>
+    import PageHeading from "$lib/components/ui/PageHeading.svelte";
+</script>
+
+<svelte:head>
+    <title>Rich Perez - Wrestling - Web Developer</title>
+</svelte:head>
+
+<section class="mx-auto max-w-2xl font-mono text-xl md:text-2xl text-center py-10">
+    <PageHeading label="Wrestling" />
+    <p>
+        I've been a big fan of pro wrestling since I was kid. From the days of Hulk Hogan to Steve Austin & The Rock and today with our Tribal Chief Roman Reigns. 
+    </p>
+</section>
+
+<section class="mx-auto max-w-2xl flex flex-col gap-2">
+    <h2 class="text-2xl font-bold font-mono">Favorite Wrestlers</h2>
+    <ol class="flex flex-col gap-2 list-decimal">
+        <li>Eddie Guerrero - <a href="https://en.wikipedia.org/wiki/Eddie_Guerrero" target="_blank">Bio</a></li>
+        <li>Scott Hall (aka Razor Ramon) - <a href="https://en.wikipedia.org/wiki/Scott_Hall" target="_blank">Bio</a></li>
+        <li>Rey Mysterio - <a href="https://en.wikipedia.org/wiki/Rey_Mysterio" target="_blank">Bio</a></li>
+        <li>Shawn Michaels - <a href="https://en.wikipedia.org/wiki/Shawn_Michaels" target="_blank">Bio</a></li>
+        <li>Booker T - <a href="https://en.wikipedia.org/wiki/Booker_T_(wrestler)" target="_blank">Bio</a></li>
+        <li>Kevin Owens - <a href="https://en.wikipedia.org/wiki/Kevin_Owens" target="_blank">Bio</a></li>
+        <li>Brodie Lee - <a href="https://en.wikipedia.org/wiki/Brodie_Lee" target="_blank">Bio</a></li>
+    </ol>
+</section>
+
+<section class="mx-auto max-w-2xl flex flex-col gap-2 mt-10">
+    <h2 class="text-2xl font-bold font-mono">Favorite Matches</h2>
+    <ol class="flex flex-col gap-2 list-decimal">
+        <li>Eddie Guerrero vs Rey Mysterio (halloween havoc 1997) - <a href="https://www.youtube.com/watch?v=sqRYKCH8dUc" target="_blank">Watch</a></li>
+        <li>Razor Ramon vs Shawn Michaels (WrestleMania 10 Ladder Match) - <a href="https://www.youtube.com/watch?v=YegEqMalt1Y" target="_blank">Watch</a></li>
+        <li>The Rock vs Hulk Hogan (WrestleMania X8) - <a href="https://www.youtube.com/watch?v=hRg_vJvyMC8" target="_blank">Watch</a></li>
+        <li>Steve Austin vs Kevin Owens (WrestleMania 38) - <a href="https://www.youtube.com/watch?v=Wukhr0uKgIE" target="_blank">Watch</a></li>
+        <li>The Wyatt Family vs The Shield (Elimination Chamber 2014) - <a href="https://www.youtube.com/watch?v=oxdKQRPIjgw" target="_blank">Watch</a></li>
+    </ol>
+</section>
