feaae5e36ed5187b21491ba37e0611890482af93
Rich Perez <imperez5@gmail.com>
2024-06-29 12:17:01 -0400

    cleaned up site logo and footer

--- src/lib/components/SiteHeader.svelte
+++ src/lib/components/SiteHeader.svelte
@@ -1,6 +1,6 @@
-<header class="flex flex-col md:flex-row items-center md:justify-between px-4 py-4 md:py-0 border-b border-slate-500">
+<header class="flex flex-col md:flex-row items-center md:justify-between px-4 py-4 border-b border-slate-500">
   <a href="/">
-    <img src="/img/site-logo.png" alt="site logo" width="300" height="86" />
+    <img src="/img/site-logo.png" alt="site logo" width="300" height="46" />
   </a>
   <nav class="flex gap-2 justify-center">
     <a class="btn btn-link text-white" href="/about">About</a>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,13 +9,13 @@
 
 <div class="flex flex-col gap-20">
   <section class="mx-auto max-w-2xl font-mono text-xl md:text-2xl text-center">
-    <img class="rounded-full mx-auto my-10" src="/img/rich.jpg" alt="rich perez" width="100" height="100" />
+    <img class="rounded-full mx-auto my-6 md:my-10" src="/img/rich.jpg" alt="rich perez" width="100" height="100" />
     <p>
       👋🏽 My name is <a class="font-black text-stone-200" href="/about">Rich Perez</a> and welcome to my space on the internet. I'm a dad, <a href="/wrestling">wrestling</a> fan and I enjoy creating web products for clients and my own side projects.
     </p>
   </section>
   <section class="flex flex-col gap-4">
-    <h2 class="text-3xl font-bold font-mono text-center border-b border-slate-500 pb-4">What I&apos;ve worked on</h2>
+    <h2 class="text-3xl font-bold font-mono text-center border-b border-slate-500 pb-4">My Work</h2>
     <div class="flex flex-col gap-10">
       {#each all() as project (project.title)}
           <Project project={project} />
