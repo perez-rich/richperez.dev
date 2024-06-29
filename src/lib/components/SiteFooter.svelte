0f74add0b84761707e967b49ccd1eb12a0b5553c
Rich Perez <imperez5@gmail.com>
2024-06-29 11:55:38 -0400

    updated site welcome header. fixed A11y img errors. centered footer text.

--- src/lib/components/SiteFooter.svelte
+++ src/lib/components/SiteFooter.svelte
@@ -1,8 +1,8 @@
-<footer class="flex flex-col gap-2 border-t border-t-stone-700 p-4 mt-10">
+<footer class="flex flex-col text-center gap-2 border-t border-t-stone-700 p-4 mt-10">
     <div>
         <p>Made by Rich with <a href="https://kit.svelte.dev" target="_blank">SvelteKit</a> & <a href="https://daisyui.com" target="_blank">DaisyUI</a>.</p>
     </div>
-    <ul class="flex gap-4">
+    <ul class="flex mx-auto gap-4">
         <li class="text-stone-400 font-bold">Slash Pages</li>
         <li><a href="/colophon">Colophon</a></li>
         <li><a href="/uses">Uses</a></li>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,9 +9,9 @@
 
 <div class="flex flex-col gap-20">
   <section class="mx-auto max-w-2xl font-mono text-xl md:text-2xl text-center">
-    <img class="rounded-full mx-auto my-10" src="/img/rich.jpg" alt="picture of rich perez" width="100" height="100" />
+    <img class="rounded-full mx-auto my-10" src="/img/rich.jpg" alt="rich perez" width="100" height="100" />
     <p>
-      Hello 👋🏽 my name is <strong class="font-black text-white">Rich Perez</strong> and I am a Full-Stack Developer. I have built websites, CMS, and REST APIs for large and small companies.
+      👋🏽 My name is <a class="font-black text-stone-200" href="/about">Rich Perez</a> and welcome to my space on the internet. I'm a dad, <a href="/wrestling">wrestling</a> fan and I enjoy creating web products for clients and my own side projects.
     </p>
   </section>
   <section class="flex flex-col gap-4">
