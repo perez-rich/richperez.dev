a8d356c4db4b33cc22d08f47c7789819745f909e
Rich Perez <imperez5@gmail.com>
2024-11-23 10:31:44 -0500

    updated projects

--- src/lib/app/repositories/data/projects.ts
+++ src/lib/app/repositories/data/projects.ts
@@ -1,32 +1,5 @@
 const json: Array<ProjectItem> = [
     {
-        type: 'client',
-        title: 'Specialty Lighting',
-        url: 'https://www.specialty-lighting.com',
-        image: '/img/projects/specialtylighting.jpg',
-        description: 'I worked with Specialty Lighting to rebuild their public website and give them an actual CMS. Their main goal was to be able to manage and configure their large volume of lighting fixture products and update their website as needed.',
-        skills: ['css', 'html', 'js', 'vue', 'mysql', 'laravel'],
-        areas: ['backend', 'frontend']
-    },
-    {
-        type: 'client',
-        title: 'FA Mag',
-        url: 'https://www.fa-mag.com',
-        image: '/img/projects/famag.jpg',
-        description: 'FA Mag is a financial news website. I regularly provide support updates and any needed improvements.',
-        skills: ['css', 'html', 'js', 'mysql', 'php'],
-        areas: ['backend', 'frontend']
-    },
-    {
-        type: 'client',
-        title: 'Gifthorse',
-        url: 'https://www.gifthorse.co',
-        image: '/img/projects/gifthorse.jpg',
-        description: 'The team at Gifthorse wanted to create a crowdsourced electronic birthday card. I worked with them to establish wireframes and design and build out all of the essential details for their MVP product launch.',
-        skills: ['css', 'html', 'js', 'vue', 'mysql', 'laravel'],
-        areas: ['backend', 'frontend']
-    },
-    {
         type: 'side project',
         title: 'The Daily Smark',
         url: 'https://www.thedailysmark.com/',
