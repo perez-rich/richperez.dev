01323fc0295989c9058d331de3c84203fd255918
Rich Perez <imperez5@gmail.com>
2025-02-27 19:27:06 -0500

    upgraded packages

--- src/lib/app/repositories/data/projects.ts
+++ src/lib/app/repositories/data/projects.ts
@@ -1,18 +1,18 @@
 const json: Array<ProjectItem> = [
     {
-        title: "The Daily Smark",
-        url: "https://www.thedailysmark.com/",
-        image: "/img/projects/thedailysmark.jpg",
+        title: 'The Daily Smark',
+        url: 'https://www.thedailysmark.com/',
+        image: '/img/projects/thedailysmark.jpg',
     },
     {
-        title: "Tonight Wrestling",
-        url: "https://www.tonightwrestling.com/",
-        image: "/img/projects/tonightwrestling.png",
+        title: 'Tonight Wrestling',
+        url: 'https://www.tonightwrestling.com/',
+        image: '/img/projects/tonightwrestling.png',
     },
     {
-        title: "Heels vs Faces",
-        url: "https://www.heelsvsfaces.com/",
-        image: "/img/projects/heelsvsfaces.png",
+        title: 'Heels vs Faces',
+        url: 'https://www.heelsvsfaces.com/',
+        image: '/img/projects/heelsvsfaces.png',
     },
 ];
 
